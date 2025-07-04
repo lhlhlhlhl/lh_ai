c680891891b70dd83a46f87edf3595d71591a8d2
lhlhlhlhl <1622803054@qq.com>
2025-07-04 21:13:03 +0800

    拍照识别单词之项目结构梳理讲解

---
+++ react/shotword/src/components/PictureCard/index.jsx
@@ -0,0 +1,42 @@
+import './style.css';
+import { useState } from 'react';
+const PictureCard = (props)=>{
+    const {
+        word,
+        uploadImg,//回调函数
+        audio
+    } = props
+    const [imgPreview,setImgPreview]= useState('https://res.bearbobo.com/resource/upload/W44yyxvl/upload-ih56twxirei.png')
+    const uploadeImgData = (e) => {
+        const file = (e.target).files?.[0];
+        if (!file) { return; }
+        return new Promise((resolve, reject) => {
+            const reader = new FileReader();//html5
+            reader.readAsDataURL(file);//读取文件
+            reader.onload = () => {
+                const data = reader.result;//base64编码
+                setImgPreview(data);
+                uploadImg(data);//上报老板
+                resolve(data);
+            }
+            reader.onerror = (error) => { reject(error); };
+        })
+    }
+
+    return (
+        <div className="card">
+            <input 
+            type="file" 
+            id="selectImage"
+            accept=".jpg,.jpeg,.png,.gif"
+            onChange={uploadeImgData}
+            />
+            <label className="upload" htmlFor="selectImage">
+                <img src={imgPreview} alt="preview" />
+            {/* htmlFor="selectImage"点击标签时会自动触发文件选择对话框 */}
+            </label>
+            <div className="word">{word}</div>
+        </div>
+    )
+}
+export default PictureCard;
